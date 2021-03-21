3f4fbf4f181b5dacf0ce170c691263cb819b6ad4
leorafink1 <fink.le@husky.neu.edu>
2021-03-21 16:09:10 -0400

    widget id

---
+++ src/main/java/com/example/webdev/controllers/WidgetController.java
@@ -0,0 +1,28 @@
+package com.example.webdev.controllers;
+
+import com.example.webdev.models.Widget;
+import com.example.webdev.services.WidgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class WidgetController {
+    @Autowired
+    WidgetService service; //= new WidgetService();
+
+    @GetMapping("/api/widgets")
+    public List<Widget> findAllWidgets() {
+        return service.findAllWidgets();
+    }
+
+    @GetMapping("/api/widgets/{wid}")
+    public Widget findWidgetById(
+            @PathVariable("wid") Long id) {
+        return service.findWidgetById(id);
+    }
+
+}
--- src/main/java/com/example/webdev/services/WidgetService.java
+++ src/main/java/com/example/webdev/services/WidgetService.java
@@ -1,11 +1,13 @@
 package com.example.webdev.services;
 
 import com.example.webdev.models.Widget;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Service
 public class WidgetService {
     private List<Widget> widgets = new ArrayList<Widget>();
     {
